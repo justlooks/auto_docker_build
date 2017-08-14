d4e32eb87e778203d739674ce3643e761f142873
huangchunhui <Alex@MacBook-Pro-2.local>
2017-08-14 11:04:50 +0800

    modify script

--- demo-build/build-ftp-server/vsftpd.sh
+++ demo-build/build-ftp-server/vsftpd.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-/bin/echo "pasv_max_port=$PASV_MAX" >> /etc/vsftpd/vsftpd.conf
-/bin/echo "pasv_min_port=$PASV_MIN" >> /etc/vsftpd/vsftpd.conf
-/bin/echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftpd/vsftpd.conf
+#/bin/echo "pasv_max_port=$PASV_MAX" >> /etc/vsftpd/vsftpd.conf
+#/bin/echo "pasv_min_port=$PASV_MIN" >> /etc/vsftpd/vsftpd.conf
+#/bin/echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftpd/vsftpd.conf
 
 
-/usr/sbin/groupadd ftpgroup
-/usr/sbin/useradd $FTP_USER -p $FTP_PASS -g ftpgroup -d /home/$FTP_USER -s /bin/false
+/usr/sbin/groupadd $FTP_USER
+/usr/sbin/useradd -g $FTP_USER -d /var/ftp/pub -s /sbin/nologin $FTP_USER
+
+echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
+chown $FTP_USER:$FTP_USER /var/ftp/pub -R
 
 &>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
